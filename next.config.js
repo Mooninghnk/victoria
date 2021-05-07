04d1c8ddec8af5ea3de41924d044318f2034430e
Totoro <33295757+Mooninghnk@users.noreply.github.com>
2021-05-06 23:58:35 -0400

    next login tsx work

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// require next-images package
+const nextImages = require("next-images");
+
+// export the module
+module.exports = nextImages({
+  // options
+  esModule: true,
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
-import Head from 'next/head'
-
 export default function Home() {
   return (
     <div>
-      
+      <h1> hello </h1>
     </div>
   )
 }
