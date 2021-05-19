a8923bc3b5bd1f5defc2995b8d516e16db7c750e
Totoro <33295757+Mooninghnk@users.noreply.github.com>
2021-05-19 15:29:50 -0400

    login added

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,19 +1,63 @@
 import { Container } from "../components/Container.js";
 import { DarkModeSwitch } from "../components/DarkModeSwitch.js";
 import { FcAddDatabase, FcNext } from "react-icons/fc";
-import { Flex, Heading } from "@chakra-ui/react";
-
+import {
+  Flex,
+  Heading,
+  Input,
+  Button,
+  useColorModeValue,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
+import { useState } from "react";
 import img from "../public/icons/background.svg";
+
 const Login = () => {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+  
   return (
     <Container
       style={{ backgroundImage: `url(${img})` }}
       height="100vh"
       aling="center"
       justifyContent="center"
+      color="black"
     >
-      <Flex direction="column" background="gray.100" p={12} rounded={6}>
-        <Heading mb={6}>Login</Heading>
+      <Flex
+        direction="column"
+        bg="white"    
+        p={20}
+        rounded={6}
+      >
+        <Heading   mb={6}>Login</Heading>
+        <br/>
+        <Input
+          placeholder="email@gmail.com"
+          variant="filled"
+          mb={3}
+          type="email"
+        />
+        <br/>
+        <InputGroup size="md">
+          <Input
+            placeholder="**********"
+            variant="filled"
+            mb={6}
+            type={show ? "text" : "password"}
+          />
+          
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+
+        <Button background="#2A019E" p={5} color="white">
+          Log In
+        </Button>
       </Flex>
 
       <DarkModeSwitch />
