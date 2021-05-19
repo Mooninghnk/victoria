ec38193897611dbbb3da2bd0dcc4af1316f76c61
Totoro <33295757+Mooninghnk@users.noreply.github.com>
2021-05-19 13:20:46 -0400

    updating login with flex

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,103 +1,21 @@
 import { Container } from "../components/Container.js";
 import { DarkModeSwitch } from "../components/DarkModeSwitch.js";
-import reg from "../public/icons/verify.svg";
-import {
-  Heading,
-  Box,
-  Flex,
-  Text,
-  useColorMode,
-  Center,
-  Input,
-  Button,
-  Grid,
-  GridItem,
-} from "@chakra-ui/react";
-import img from "../public/icons/background.svg";
-import { useState } from "react";
 import { FcAddDatabase, FcNext } from "react-icons/fc";
-const Login = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
-  const isDark = colorMode === "dark";
-  const [set, setSet] = useState("none");
+import { Flex, Heading } from "@chakra-ui/react";
 
+import img from "../public/icons/background.svg";
+const Login = () => {
   return (
     <Container
       style={{ backgroundImage: `url(${img})` }}
-      py={300}
       height="100vh"
+      aling="center"
+      justifyContent="center"
     >
-      <Center display="flex" h="100px">
-        <Flex align="center" display="flex" color="black">
-          <Box
-            maxW={"450px"}
-            w={"full"}
-            boxShadow={"2xl"}
-            rounded={"md"}
-            overflow={"hidden"}
-            w="100%"
-            rounded="lg"
-            borderRadius="lg"
-            boxShadow={"2xl"}
-            rounded={"md"}
-            display="fixed"
-            background="white"
-            align="center"
-            p={10}
-          >
-            <Box p={20} m="200" w="100%" as="a" align="center">
-              <Heading m="1" mb="0" as="h2" color="black" size="lg">
-                Login
-              </Heading>
-              <br />
-              <Box>
-                <Button
-                  leftIcon={<FcAddDatabase />}
-                  background="#EFF0F6"
-                  color="black"
-                  variant="solid"
-                >
-                  Sign Up
-                </Button>
-              </Box>
-              <br />
-              <Box>
-                <Input
-                  textAlign="center"
-                  type="email"
-                  placeholder="email@gmail.com"
-                  background="#EFF0F6"
-                  variant="filled"
-                  color="black"
-                />
-              </Box>{" "}
-              <br />
-              <Box>
-                <Input
-                  textAlign="center"
-                  type="password"
-                  placeholder="password"
-                  background="#EFF0F6"
-                  variant="filled"
-                  color="black"
-                />
-              </Box>
-              <br />
-              <Box>
-                <Button
-                  marginTop="1em"
-                  leftIcon={<FcAddDatabase />}
-                  background="#5F2EEA"
-                  color="white"
-                  variant="solid"
-                >
-                  Login
-                </Button>
-              </Box>
-            </Box>
-          </Box>
-        </Flex>
-      </Center>
+      <Flex direction="column" background="gray.100" p={12} rounded={6}>
+        <Heading mb={6}>Login</Heading>
+      </Flex>
+
       <DarkModeSwitch />
     </Container>
   );
