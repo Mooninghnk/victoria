0dd0c16bb35f4bac19b3d799822a0e4a016f4f33
Totoro <33295757+Mooninghnk@users.noreply.github.com>
2021-05-19 12:59:12 -0400

    changed the ui

--- src/pages/login.js
+++ src/pages/login.js
@@ -30,6 +30,7 @@ const Login = () => {
       <Center display="flex" h="100px">
         <Flex align="center" display="flex" color="black">
           <Box
+            w="100%"
             rounded="lg"
             borderRadius="lg"
             boxShadow={"2xl"}
@@ -37,55 +38,57 @@ const Login = () => {
             display="fixed"
             background="white"
             align="center"
-            p={8}
+            p={10}
           >
-            <Box m="300" as="a" align="center">
+            <Box p={20} m="200" w="100%" as="a" align="center">
               <Heading m="1" mb="0" as="h2" color="black" size="lg">
                 Login
               </Heading>
-              <br />
-              <br />
-              <Button
-                leftIcon={<FcAddDatabase />}
-                background="#EFF0F6"
-                color="black"
-                variant="solid"
-              >
-                Sign Up
-              </Button>
-              <br />
-              <br />
-              <Input
-                textAlign="center"
-                type="email"
-                placeholder="email@gmail.com"
-                background="#EFF0F6"
-                variant="filled"
-                color="black"
-              />
-              <br />
-              <br />
-              <Input
-                textAlign="center"
-                type="password"
-                placeholder="password"
-                background="#EFF0F6"
-                variant="filled"
-                color="black"
-              />
-              <br />
-              <br />
-              <Center>
+              <br/>
+              <Box>
                 <Button
-                  textAlign="center"
-                  leftIcon={<FcNext />}
+                  leftIcon={<FcAddDatabase />}
                   background="#EFF0F6"
                   color="black"
                   variant="solid"
                 >
+                  Sign Up
+                </Button>
+              </Box>
+              <br/>
+              <Box>
+                <Input
+                  textAlign="center"
+                  type="email"
+                  placeholder="email@gmail.com"
+                  background="#EFF0F6"
+                  variant="filled"
+                  color="black"
+                />
+              </Box>{" "}
+              <br/>
+              <Box>
+                <Input
+                  textAlign="center"
+                  type="password"
+                  placeholder="password"
+                  background="#EFF0F6"
+                  variant="filled"
+                  color="black"
+                />
+              </Box>
+              <br/>
+              <Box>
+                <Button
+                  marginTop="1em"
+                  leftIcon={<FcAddDatabase />}
+                  background="#5F2EEA"
+                  color="white"
+                  variant="solid"
+                >
                   Login
                 </Button>
-              </Center>
+              </Box>
             </Box>
           </Box>
         </Flex>
